6e050c1e58e9893c8c65aa95b4bcab5719a218bc
ngchaksan <ngchaksan@ngchaksans-MacBook-Air.local>
2022-10-12 17:56:13 +0800

    Lesson10&11

---
+++ Testing.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Testing {
+
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int c = scan.nextInt();
+
+        double b = scan.nextDouble();
+       
+        String a = scan.nextLine();
+        a = scan.nextLine();
+
+        
+        
+    
+        
+         System.out.println("String: " + a);
+         System.out.println("Double: " + b);
+         System.out.println("Int: " + c);
+  
+}
+  }
